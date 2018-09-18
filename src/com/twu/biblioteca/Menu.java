be3689434e7d9b2ae7a5279ee44548f640336d5e
Paula <paula@Paulas-MacBook-Pro.local>
2018-09-18 14:37:32 +0100

    Refactored Menu to remove duplicate code

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -13,7 +13,7 @@ public class Menu {
         this.library = library;
     }
 
-    void display() {
+    public void display() {
         System.out.println("Choose one of the following options:");
         System.out.println("    1) List of all books");
         System.out.println("    2) Checkout book");
@@ -21,7 +21,7 @@ public class Menu {
         System.out.println("    4) Quit");
     }
 
-    void chooseOption() {
+    public void chooseOption() {
         int choice = takeInput.nextInt();
         if (this.checkIfValid(choice)) {
             switch (choice) {
@@ -32,35 +32,22 @@ public class Menu {
                     break;
                 }
                 case 2: {
-                    System.out.println("What is the title of the book you would like to checkout?");
-                    String title = takeInput.next();
-                    System.out.println("Who is the author of the book you would like to checkout?");
-                    String author = takeInput.next();
-                    System.out.println("When was this book published?");
-                    int year = takeInput.nextInt();
-                    Book b = new Book(title, author, year);
+                    Book b = getBookDetails();
                     library.checkoutBook(b, listOfBooks);
                     this.display();
                     this.chooseOption();
                     break;
                 }
                 case 3: {
-                    System.out.println("What is the title of the book you would like to return?");
-                    String title = takeInput.next();
-                    System.out.println("Who is the author of the book you would like to return?");
-                    String author = takeInput.next();
-                    System.out.println("When was this book published?");
-                    int year = takeInput.nextInt();
-                    Book b = new Book(title, author, year);
+                    Book b = getBookDetails();
                     library.returnBook(b, listOfBooks);
                     this.display();
                     this.chooseOption();
                     break;
                 }
-
-
             }
         }
+
         else {
             System.out.println("Select a valid option!");
             this.display();
@@ -68,10 +55,19 @@ public class Menu {
         }
     }
 
+
     public boolean checkIfValid(int choice) {
-        if (choice == 1 || choice == 2 || choice == 3 || choice == 4)
-            return true;
-        else
-            return false;
+        return (choice == 1 || choice == 2 || choice == 3 || choice == 4);
+    }
+
+
+    private Book getBookDetails() {
+        System.out.println("What is the title of the book you would like to checkout?");
+        String title = takeInput.next();
+        System.out.println("Who is the author of the book you would like to checkout?");
+        String author = takeInput.next();
+        System.out.println("When was this book published?");
+        int year = takeInput.nextInt();
+        return new Book(title, author, year);
     }
 }
