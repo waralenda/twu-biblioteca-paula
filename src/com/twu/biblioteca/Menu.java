ce888f8aa4bbe0eed99c11e7a294130ddb0cf40a
Paula <paula@Paulas-MacBook-Pro.local>
2018-09-18 09:17:49 +0100

    Menu display

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -2,6 +2,14 @@ package com.twu.biblioteca;
 
 public class Menu {
 
+    void display() {
+        System.out.println("Choose one of the following options:");
+        System.out.println("    1) List of all books");
+        System.out.println("    2) Checkout book");
+        System.out.println("    3) Return book");
+    }
+
+    
 
 
 }
